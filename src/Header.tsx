591117cc29181938a4ecfccabd65e34e3b6b39f4
Torben Rahbek Koch <torben@rahbekkoch.dk>
2022-03-02 10:48:31 +0100

    Mobile-friendly header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,21 @@
 import styled from 'styled-components';
 
-const MugShot = styled.img`
+const MugShot = styled.picture`
   position: absolute;
-  left: 10%;
-  top: 15px;
+  @media screen and (max-width: 1000px) {
+  left: 20%;
+  top: 10px;
+  margin-bottom: -72px;
+  }
+  @media screen and (min-width: 1000px) {
+    left: 20%;
+    top: 15px;    
+  }
+  @media screen and (max-width: 600px) {
+  left: 10px;
+  top: 10px;
+  margin-bottom: -72px;
+  }
 `
 const StyledHeader = styled.h1`
   margin-top: 10px;
@@ -11,12 +23,16 @@ const StyledHeader = styled.h1`
   font-style: italic;
   font-size: 2.5em;
   font-weight: bold;
+  padding-left: 72px;
 `
 export function Header() {
   return (
     <div>
-      <MugShot src="/images/mugshot.jpg"/>
       <StyledHeader>Mr. Legacy</StyledHeader>
+      <MugShot>
+        <source srcSet="/images/mugshot.jpg" media="(min-width: 1000px)"/>
+        <img src="/images/mugshot-small.jpg" alt=""/>
+      </MugShot>
       <p>aka Torben Koch Pløen, +45 2482 1824, torben at kochploeen.dk</p>
     </div>
   )
--- src/Profile/Style.ts
+++ src/Profile/Style.ts
@@ -3,4 +3,8 @@ import styled from 'styled-components'
 export const Profile = styled.div`
   width: 60%;
   font-size: 1.5em;
+  text-align: left;
+  @media screen and (max-width: 600px) {
+    font-size: 1.2em;
+  }
 `
